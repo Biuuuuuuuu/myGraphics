4e890a743e797f48867cf43526f722ce63894ddb
Biuuuuuuuu <biuuuuuuuu@hotmail.com>
2018-04-24 18:07:30 +0800

    椭圆

---
+++ Project2/Project2/main.cpp
@@ -0,0 +1,110 @@
+#ifndef UNICODE
+#define UNICODE
+#endif 
+
+#include <windows.h>
+#include "myGraphics.hpp"
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int x, y;
+long c;
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
+{
+	// Register the window class.
+	const wchar_t className[] = L"Sample Window Class";
+
+	WNDCLASS wc = {};
+
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = className;
+
+	RegisterClass(&wc);
+
+	// Create the window.
+
+	HWND hwnd = CreateWindowEx(
+		0,                              // Optional window styles.
+		className,                     // Window class
+		L"绘制",    // Window text
+		WS_OVERLAPPEDWINDOW,            // Window style
+
+										// Size and position
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+		NULL,       // Parent window    
+		NULL,       // Menu
+		hInstance,  // Instance handle
+		NULL        // Additional application data
+	);
+
+	if (hwnd == NULL)
+	{
+		return 0;
+	}
+
+	ShowWindow(hwnd, nCmdShow);
+
+	
+	// Run the message loop.
+
+	MSG msg = {};
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	case WM_CLOSE:
+		if (MessageBox(hwnd, L"确认退出?", L"退出...", MB_OKCANCEL) == IDOK)
+		{
+			DestroyWindow(hwnd);
+		}
+		// Else: User canceled. Do nothing.
+		return 0;
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hwnd, &ps);
+		//涂黑背景
+		RECT fullRect;
+		fullRect.bottom = 0;
+		fullRect.left = 0;
+		fullRect.right = 2560;
+		fullRect.top = 1080;
+		FillRect(hdc, &fullRect, CreateSolidBrush(RGB(0, 0, 0)));
+				
+		//绘制
+		for (int i = 0; i <= 600; i += 100) {
+			drawLine(hdc, 300, 300, 600, i, RGB(i, 0, 255));
+			drawLine(hdc, 300, 300, 0, i, RGB(i, 255, 0));
+			drawLine(hdc, 300, 300, i, 0, RGB(0, i, 255));
+			drawLine(hdc, 300, 300, i, 600, RGB(255, i, 0));
+		}
+
+		drawCircle(hdc, 300, 300, 200, RGB(255, 100, 100));
+		drawCircle1(hdc, 300, 300, 250, RGB(100, 100, 255));
+		for(int i=40;i<=400;i+=40)
+			drawEllipse(hdc, 700, 200+2*(i-100), i, 50, RGB(100, (i-200)*2, 50));
+		for (int i = 40; i <=400; i += 40)
+			drawEllipse(hdc, 200 + 2 * (i - 100), 700, 50, i, RGB((i - 200) * 2, 50, 100));
+		
+		EndPaint(hwnd, &ps);
+	}
+	return 0;
+
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
